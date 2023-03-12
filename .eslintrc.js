82d89a1500bbee0cc199df6b50bb0bb6f369b7fc
DetachHead <detachhead@users.noreply.github.com>
2023-03-12 10:56:02 +1000

    use `eslint-plugin-eslint-plugin`

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,9 @@
 /** @type {import('eslint').Linter.Config} */
 const config = {
-    extends: ['@detachhead/eslint-config'],
+    extends: ['@detachhead/eslint-config', 'plugin:eslint-plugin/all'],
+    rules: {
+        'eslint-plugin/require-meta-docs-url': 'off', // i'm using md links on github which is the default
+    },
     overrides: [
         {
             files: ['src/**/*.ts'],
