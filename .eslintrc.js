01faa47266a9a99562be69bc79324f2067445524
DetachHead <detachhead@users.noreply.github.com>
2023-04-19 00:04:44 +1000

    ignore "File is a CommonJS module; it may be converted to an ES module" cuz esmodules are a pain since most tools don't support them

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,13 @@ const config = {
     extends: ['@detachhead/eslint-config', 'plugin:eslint-plugin/all'],
     rules: {
         'eslint-plugin/require-meta-docs-url': 'off', // i'm using md links on github which is the default
+        'detachhead/suggestions-as-errors': [
+            'error',
+            {
+                // TODO: remove once eslint-config is updated with this
+                exclude: [80001], // File is a CommonJS module; it may be converted to an ES module
+            },
+        ],
     },
     overrides: [
         {
