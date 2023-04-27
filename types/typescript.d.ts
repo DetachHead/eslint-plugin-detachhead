b8305b28ea4fe922d34bec29f98218dc3efbbec9
DetachHead <detachhead@users.noreply.github.com>
2023-04-27 22:57:47 +1000

    typecheck tests and config files

--- types/typescript.d.ts
+++ types/typescript.d.ts
@@ -1,4 +1,4 @@
-// eslint-disable-next-line detachhead/suggestions-as-errors -- https://github.com/microsoft/TypeScript/issues/54042
+// https://github.com/microsoft/TypeScript/issues/54042
 import { CancellationToken, DiagnosticWithLocation, Program, SourceFile } from 'typescript'
 
 declare module 'typescript' {
